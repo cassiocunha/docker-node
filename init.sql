65af4457532479679137d1036fe85206804c8992
cassiocunha <cassio1706@gmail.com>
2023-02-14 20:10:43 -0600

    first commit

---
+++ init.sql
@@ -0,0 +1,4 @@
+create table people (id int not null auto_increment, name varchar(255), primary key(id));
+insert into people(name) values('Jose');
+insert into people(name) values('Maria');
+insert into people(name) values('Diego');
---
+++ node/index.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const mysql = require('mysql');
+const app = express();
+const port = 3000;
+
+const config = {
+    host: 'db',
+    user: 'root',
+    password: 'root',
+    database: 'nodedb'
+};
+
+const con = mysql.createConnection(config);
+const sql = 'select * from people';
+let peopleList = [];
+con.connect(function (err) {
+    if (err) throw err;
+    console.log("Connected!");
+    con.query(sql, function (err, result) {
+        if (err) throw err;
+        peopleList = result;
+    });
+});
+
+app.get('/', (req, res) => {
+    res.send(`<h1>Full Cycle Rocks!</h1> <ul> ${peopleList.map(item => `<li>${item.name} </li>`).join('')} </ul>`);
+});
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+});
+
+
+
+
+
+
